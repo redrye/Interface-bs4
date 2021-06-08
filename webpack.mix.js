494a584799984da474790b70334dd88e7368f856
Ryan Boudreaux <boudreauxryan@gmail.com>
2021-06-08 12:06:34 -0500

    env variables and misc configs.

---
+++ webpack.mix.js
@@ -0,0 +1,40 @@
+const Dotenv = require('dotenv-webpack');
+const mix = require('laravel-mix');
+const webpack = require('webpack');
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+
+mix.webpackConfig({
+	externals: function (context, request, callback) {
+		if (/xlsx|canvg|pdfmake|pdfMake/.test(request)) {
+			return callback(null, "commonjs " + request);
+		}
+		callback();
+	},
+	plugins: [
+		new Dotenv({
+			path: './.env',
+			safe: true
+		})
+	]
+})
+
+
+
+mix.sass('resources/sass/bootstrap.scss', 'public/css');
+
+mix.js('resources/js/app.js', 'public/js').extract();
+
+mix.js('resources/js/sync-address.js', 'public/js');
+mix.js('resources/js/jobdash-ro.js', 'public/js');
+mix.js('resources/js/statecountry.js', 'public/js');
+
